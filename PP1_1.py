d65b05500daba699db6b1482fc7757f7d7ba407e
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-11-05 17:41:44 -0500

    Update PP1_1.py

--- PP1_1.py
+++ PP1_1.py
@@ -4,23 +4,20 @@ def q1():
 def q2():
   #Write Assignment code here
   print(1)
-
   print(2)
-
   print(3)
-
   print(4)
-
   print(5)
 def q3():
   #Write Assignment code here
-  print( "Help me!")
+  print(" Help Me!")
   
 def q4():
   #Write Assignment code here
-  print("3x3=9")
+  print("3 x 3 = 9")
 
-q1()
-q2()
-q3()
-q4()
+# q1()
+# q2()
+# q3()
+# q4()
+
